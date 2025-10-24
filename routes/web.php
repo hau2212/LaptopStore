1a796a3290ae1fbeb40a9183af1a4c76466925a6
Khangcyber <khangnt24@saigontech.edu.vn>
2025-10-24 11:35:48 +0700

    hau

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home' , 'App\Http\Controllers\homeController@index')->name('home');
 
 Route::get('/show/{id}' , 'App\Http\Controllers\productsController@show')->name('product.show');
-       
-
-
-
-
-
-
+    
 
 ?>
