5d5e1e4591cfe215c53aa00ce8508201078f8a42
Khangcyber <khangnt24@saigontech.edu.vn>
2025-10-24 11:24:25 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home' , 'App\Http\Controllers\homeController@index')->name('home');
 
 Route::get('/show/{id}' , 'App\Http\Controllers\productsController@show')->name('product.show');
+       
+
+
+
+
+
+
 
 ?>
